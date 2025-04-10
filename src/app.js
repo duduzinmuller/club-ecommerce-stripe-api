e1ce153c37ed80ac80fb03ede11519a232890fb7
Eduardo <eduardomuller989@gmail.com>
2025-04-10 17:54:23 -0400

    add all

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://eccomerce-frontend-ten.vercel.app",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 };
